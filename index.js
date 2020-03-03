f1aecdb74f1bbcfb837c2ba976d78d2e964faf0c
Andrey Fomin <a48671@gmail.com>
2020-03-03 15:53:17 +0500

    replace

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const path = require('path');
 
 const PORT = 3010;
 
-const DATA = 'DATA';
+const DATA = {data: 'DATA'};
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
     res.setHeader("Content-Type", "text/html; charset=utf-8;");
     fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
         if (err) throw new Error(err);
-        res.write(data);
+        const html = data.toString().replace('{{DATA}}', JSON.stringify(DATA));
+        res.write(html);
         res.end();
     });
 });
