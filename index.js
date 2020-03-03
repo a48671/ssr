eb1fbaaf9b18f0270dd9b877679556241f362e8d
Andrey Fomin <a48671@gmail.com>
2020-03-03 16:44:58 +0500

    get users

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
 
 const PORT = 3010;
-
-const DATA = {data: 'DATA'};
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
     res.setHeader("Content-Type", "text/html; charset=utf-8;");
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    fs.readFile(path.join(__dirname, 'index.html'), async (err, data) => {
         if (err) throw new Error(err);
-        const html = data.toString().replace('{{DATA}}', JSON.stringify(DATA));
+        const users = await axios.get('https://api.github.com/users').then(response => response.data).catch(() => []);
+        const html = data.toString().replace('{{DATA}}', JSON.stringify(users));
         res.write(html);
         res.end();
     });
